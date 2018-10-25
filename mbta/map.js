ce71ebee03e9f2c7b2a0c9dc8fb1fa4328551551
Leila Li <jiaxun.li@tufts.edu>
2018-10-25 02:06:07 -0400

    can't get the schedule to work, maybe it's too late

--- mbta/map.js
+++ mbta/map.js
@@ -10,30 +10,30 @@ var infoWindow = new google.maps.InfoWindow();
 
 var MBTAStops = 
 [ 
-  { "stop_name":"Alewife","stop_lat":42.395428,"stop_lon":-71.142483,"stop_id":"place-alfcl"},
-  { "stop_name":"Davis","stop_lat":42.39674,"stop_lon":-71.121815,"stop_id":"place-davis" },
-  { "stop_name":"Porter Square","stop_lat":42.3884,"stop_lon":-71.11914899999999,"stop_id":"place-portr" },
-  { "stop_name":"Harvard Square","stop_lat":42.373362,"stop_lon":-71.118956,"stop_id":"place-harsq" },
-  { "stop_name":"Central Square","stop_lat":42.365486,"stop_lon":-71.103802,"stop_id":"place-cntsq" },
-  { "stop_name":"Kendall/MIT","stop_lat":42.36249079,"stop_lon":-71.08617653,"stop_id":"place-knncl" },
-  { "stop_name":"Charles/MGH","stop_lat":42.361166,"stop_lon":-71.070628,"stop_id":"place-chmnl" },
-  { "stop_name":"Park Street","stop_lat":42.35639457,"stop_lon":-71.0624242,"stop_id":"place-pktrm" },
-  { "stop_name":"Downtown Crossing","stop_lat":42.355518,"stop_lon":-71.060225,"stop_id":"place-dwnxg" },
-  { "stop_name":"South Station","stop_lat":42.352271,"stop_lon":-71.05524200000001,"stop_id":"place-sstat" },
-  { "stop_name":"Broadway","stop_lat":42.342622,"stop_lon":-71.056967,"stop_id":"place-brdwy" },
-  { "stop_name":"Andrew","stop_lat":42.330154,"stop_lon":-71.057655,"stop_id":"place-andrw" },
-  { "stop_name":"JFK/UMass","stop_lat":42.320685,"stop_lon":-71.052391,"stop_id":"place-jfk" },
-  
-  { "stop_name":"Savin Hill","stop_lat":42.31129,"stop_lon":-71.053331,"stop_id":"place-shmnl" },
-  { "stop_name":"Fields Corner","stop_lat":42.300093,"stop_lon":-71.061667,"stop_id":"place-fldcr" },
-  { "stop_name":"Shawmut","stop_lat":42.29312583,"stop_lon":-71.06573796000001,"stop_id":"place-smmnl" },
-  { "stop_name":"Ashmont","stop_lat":42.284652,"stop_lon":-71.06448899999999,"stop_id":"place-asmnl" },  
-  
-  { "stop_name":"North Quincy","stop_lat":42.275275,"stop_lon":-71.029583,"stop_id":"place-nqncy" },
-  { "stop_name":"Wollaston","stop_lat":42.2665139,"stop_lon":-71.0203369,"stop_id":"place-wlsta" },
-  { "stop_name":"Quincy Center","stop_lat":42.251809,"stop_lon":-71.005409,"stop_id":"place-qnctr" },
-  { "stop_name":"Quincy Adams","stop_lat":42.233391,"stop_lon":-71.007153,"stop_id":"place-qamnl" },
-  { "stop_name":"Braintree","stop_lat":42.2078543,"stop_lon":-71.0011385,"stop_id":"place-brntn" }
+{ "stop_name":"Alewife","stop_lat":42.395428,"stop_lon":-71.142483,"stop_id":"place-alfcl"},
+{ "stop_name":"Davis","stop_lat":42.39674,"stop_lon":-71.121815,"stop_id":"place-davis" },
+{ "stop_name":"Porter Square","stop_lat":42.3884,"stop_lon":-71.11914899999999,"stop_id":"place-portr" },
+{ "stop_name":"Harvard Square","stop_lat":42.373362,"stop_lon":-71.118956,"stop_id":"place-harsq" },
+{ "stop_name":"Central Square","stop_lat":42.365486,"stop_lon":-71.103802,"stop_id":"place-cntsq" },
+{ "stop_name":"Kendall/MIT","stop_lat":42.36249079,"stop_lon":-71.08617653,"stop_id":"place-knncl" },
+{ "stop_name":"Charles/MGH","stop_lat":42.361166,"stop_lon":-71.070628,"stop_id":"place-chmnl" },
+{ "stop_name":"Park Street","stop_lat":42.35639457,"stop_lon":-71.0624242,"stop_id":"place-pktrm" },
+{ "stop_name":"Downtown Crossing","stop_lat":42.355518,"stop_lon":-71.060225,"stop_id":"place-dwnxg" },
+{ "stop_name":"South Station","stop_lat":42.352271,"stop_lon":-71.05524200000001,"stop_id":"place-sstat" },
+{ "stop_name":"Broadway","stop_lat":42.342622,"stop_lon":-71.056967,"stop_id":"place-brdwy" },
+{ "stop_name":"Andrew","stop_lat":42.330154,"stop_lon":-71.057655,"stop_id":"place-andrw" },
+{ "stop_name":"JFK/UMass","stop_lat":42.320685,"stop_lon":-71.052391,"stop_id":"place-jfk" },
+
+{ "stop_name":"Savin Hill","stop_lat":42.31129,"stop_lon":-71.053331,"stop_id":"place-shmnl" },
+{ "stop_name":"Fields Corner","stop_lat":42.300093,"stop_lon":-71.061667,"stop_id":"place-fldcr" },
+{ "stop_name":"Shawmut","stop_lat":42.29312583,"stop_lon":-71.06573796000001,"stop_id":"place-smmnl" },
+{ "stop_name":"Ashmont","stop_lat":42.284652,"stop_lon":-71.06448899999999,"stop_id":"place-asmnl" },  
+
+{ "stop_name":"North Quincy","stop_lat":42.275275,"stop_lon":-71.029583,"stop_id":"place-nqncy" },
+{ "stop_name":"Wollaston","stop_lat":42.2665139,"stop_lon":-71.0203369,"stop_id":"place-wlsta" },
+{ "stop_name":"Quincy Center","stop_lat":42.251809,"stop_lon":-71.005409,"stop_id":"place-qnctr" },
+{ "stop_name":"Quincy Adams","stop_lat":42.233391,"stop_lon":-71.007153,"stop_id":"place-qamnl" },
+{ "stop_name":"Braintree","stop_lat":42.2078543,"stop_lon":-71.0011385,"stop_id":"place-brntn" }
 ];
 
 //Initialize and add the map
@@ -44,7 +44,7 @@ function initMap(){
       zoom: 13,              //city=10, streets=15
       center: SouthStation   //centered on south station
     }
-  );
+    );
   renderStops(map);
   getMyLocation();
 }
@@ -54,36 +54,36 @@ function getMyLocation(){
   //var marker = new google.maps.Marker ({position: SouthStation, map: map});
         if (navigator.geolocation) { // the navigator.geolocation object is supported on the browser
           navigator.geolocation.getCurrentPosition(function(position) {
-              currentLat = position.coords.latitude;
-              currentLng = position.coords.longitude;
+            currentLat = position.coords.latitude;
+            currentLng = position.coords.longitude;
               renderMyMarker();      //call funtion to render my marker
 
-            map.setCenter(currentLoc);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
+              map.setCenter(currentLoc);
+            }, function() {
+              handleLocationError(true, infoWindow, map.getCenter());
+            });
         } else {
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
         }
-}
+      }
 
 //display an infoWindow if the Geolocation service failed.
 function handleLocationError(browserHasGeolocation, infoWindow, currentLoc) {
-        infoWindow.setPosition(currentLoc);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
+  infoWindow.setPosition(currentLoc);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
 }
 
 //add Markers for all the MBTA stops and connect them with a polyline 
 function renderStops(map) {
-        var image = 'signpost.png'; 
-        var stop = {};
-        var stopLatLng;
-        var stopMarker;
-        var PolylinePath = [];
+  var image = 'signpost.png'; 
+  var stop = {};
+  var stopLatLng;
+  var stopMarker;
+  var PolylinePath = [];
 
         //add markers 
         for (var i = 0; i < 22; i++) {
@@ -94,9 +94,9 @@ function renderStops(map) {
             icon: image, 
             title: stop["stop_name"]
             //zIndex: stop["stop_id"]   
-            });
+          });
           //call function to add infoWindow event 
-          getSchedule(stopMarker, stop);
+          getSchedule(stopMarker, stop,map);
           //add the stop's coordinates to PolylinePath
           stopLatLng = new google.maps.LatLng({lat: stop["stop_lat"], lng: stop["stop_lon"]});
           PolylinePath.push(stopLatLng);
@@ -111,7 +111,7 @@ function renderStops(map) {
           strokeWeight: 2
         });
         Polyline.setMap(map);
-}
+      }
 
 //add a marker on my current location, infowindow and render it on the map 
 function renderMyMarker() {
@@ -129,19 +129,18 @@ function renderMyMarker() {
   var nearestStop = calNearest(currentLoc);
   //render a Polyline that connects current location marker to the nearest stop marker
   var Polyline = new google.maps.Polyline({
-          path: [currentLoc,nearestStop.LatLng],
-          geodesic: true,
-          strokeColor: '#0000FF',
-          strokeOpacity: 1.0,
-          strokeWeight: 3
-        });
+    path: [currentLoc,nearestStop.LatLng],
+    geodesic: true,
+    strokeColor: '#0000FF',
+    strokeOpacity: 1.0,
+    strokeWeight: 3
+  });
   Polyline.setMap(map);
 
   //Open info window on click of marker
   myMarker.addListener('click', function() {
-    
     var contentString = 
-  "The nearest MBTA Red Line subway stop is " + nearestStop.name + " which is " + nearestStop.distance + " miles away from me.";    
+    "The nearest MBTA Red Line subway stop is " + nearestStop.name + " which is " + nearestStop.distance + " miles away from me.";    
     infoWindow.setContent(contentString);
     infoWindow.open(map, myMarker);
   });  
@@ -156,7 +155,7 @@ function calNearest(LatLng) {
   var name = "";
   var smallest = Infinity; 
 
-  for (var i in MBTAStops) {
+  for (i=0;i<22;i++) {
     stop = MBTAStops[i];
     stopLatLng = new google.maps.LatLng({lat: stop.stop_lat, lng: stop.stop_lon});
     distance = google.maps.geometry.spherical.computeDistanceBetween(currentLoc, stopLatLng);
@@ -176,26 +175,34 @@ function calNearest(LatLng) {
 }
 
 //pass in a marker, which is stopMarker, no output
-function getSchedule(marker,object){
+function getSchedule(marker,object,map){
 
-    var schedule;
-    var requestURL = 'https://chicken-of-the-sea.herokuapp.com/redline/schedule.json?stop_id='+ object.stop_id;
-    var request = new XMLHttpRequest();
-    var responseText;
-    var contentString;
+  var requestURL = 'https://chicken-of-the-sea.herokuapp.com/redline/schedule.json?stop_id='+ object.stop_id;
+  var request = new XMLHttpRequest();
+  var parsed;
+  var direction;
+  var returnHTML="";
+  var contentString;
 
-    request.open('GET', requestURL, true);
+  request.open('GET', requestURL, true); //this works
 
-    request.onreadystatechange = function (){
-        if (request.readyState == 4 && request.status == 200) {
-          responseText = request.responseText;
-          schedule = JSON.parse(responseText);
-          /*var returnHTML = "<ul>";
-          for (i = 0; i < messages.length; i++) {
-            returnHTML += "<li>" + messages[i].content + " by " + messages[i].username + 
-            "</li>";
+  request.onreadystatechange = function (){
+
+    if (request.readyState == 4 && request.status == 200) {
+      //console.log(request.readyState); //that's a 4 
+      parsed = JSON.parse(request.responseText); //Parse the data with JSON.parse(), and the data becomes a JavaScript object.
+      console.log(parsed);//that's an object, but the length is 0 
+      console.log("not in the for loop yet"); //this is printed
+      console.log("parsed.data.length is "+parsed.data.length);//this is 0, so the loop does not work
+      for (i=0;i<parsed.data.length; i++) {
+        console.log("finally in the for loop"); //not printed, why not in the for loop???
+        console.log(i);
+        if (parsed.data[i].attributes.direction_id == 0) {
+          direction = "Northbound to Alewife";}
+          else if (parsed.data[i].attributes.direction_id == 1){
+            direction = "Southbound (to Ashmont/Braintree)";}
+            returnHTML += "<tr><td>"+parsed[i].arrival_time+"</td><td>" + direction + "</td></tr>";  
           }
-          returnHTML += "</ul>";*/
         }
         else if (request.readyState == 4 && request.status != 200) {
           schedule = "Schedule not available.";
@@ -203,17 +210,18 @@ function getSchedule(marker,object){
         else if (request.readyState == 3) {
           schedule = "Loading...";
         }
-    }
+      };
 
-    request.send();
-
-   contentString = 
-    "<h1>" + object.stop_name+ "</h1>" + "<p>Upcoming trains:</p>"+ schedule;   
-
-    marker.addListener('click', function() {
-    infoWindow.setContent(contentString);
-    infoWindow.open(map, marker);
+      request.send();
+      console.log(returnHTML);
+      contentString = 
+      "<h1>" + object.stop_name+ "</h1>" + "<table><tr><th>Upcoming Train</th><th>Direction</th></tr>"+ returnHTML +"</table>";   
+      console.log(contentString);
+   // marker.addListener('click', function() {
+    marker.addListener('click',function(){
+      infoWindow.setContent(contentString);
+      infoWindow.open(map,marker);
     });
-}
+  }
 
 
